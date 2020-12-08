428da94bb1fe238f80d1529a1aada90593b6a272
christopher-blackburn <70343506+christopher-blackburn@users.noreply.github.com>
2020-12-08 10:23:11 -0500

    Extract Job Data from BG XML

---
+++ prediction/getBurningJobs.py
@@ -0,0 +1,49 @@
+# getBurningJobs.py
+
+'''
+This script accesses the jobs within the Burnign Glass XML files
+'''
+
+import os
+from xml.dom import minidom 
+import pandas as pd 
+
+
+# Set the working directory to the sample XML files
+os.chdir('/Users/cblackburn/Downloads')
+
+# Parse the XML document 
+mydoc = minidom.parse('US_XML_AddFeed_20180702_20180708.xml')
+
+# Extract each "Job" element
+items = mydoc.getElementsByTagName('Job')
+
+
+
+# Next, we can iterate within the each of these to extract the relevant information.
+job_info_array = []
+for node in items:
+	try:
+		# Extract the Job ID
+		jobID = node.getElementsByTagName('JobID')[0].firstChild.nodeValue
+	
+		# Extract the raw text data from the job
+		jobText = node.getElementsByTagName('JobText')[0].firstChild.nodeValue
+
+	except AttributeError:
+		# Extract the Job ID
+		jobID = node.getElementsByTagName('JobID')[0].firstChild.nodeValue
+		
+		# Assign an empty job posting text
+		jobText = ''
+
+	# Append the job information to the job information array
+	job_info_array.append([jobID,jobText])
+
+
+# Convert to a pandas DataFrame
+job_info_df = pd.DataFrame(job_info_array,columns=['JobID','JobText'])
+
+# Save the data 
+os.chdir('/Users/cblackburn/Dropbox/Data Economy/BurnGlassData')
+job_info_df.to_csv('burn_glass_test.csv',index=False)
